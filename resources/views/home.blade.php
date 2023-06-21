d21abe55fed134a05e90b02b346634ec82fc408d
Hashaam Sheikh <hashaamsheikh@hotmail.co.uk>
2023-06-21 17:29:35 +0100

    Created model, controller and migration with 1 command

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,51 @@
                     {{ __('You are logged in!') }}
                 </div>
             </div>
+            <div class="mt-4 mb-4 d-flex align-items-center justify-content-between">
+                <h5 class="m-0">Polls</h5>
+                <button type="button" class="btn btn-primary">New Poll +</button>
+            </div>
+                <div>   
+                <table class="table table-bordered">
+                    <thead>
+                      <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Slug</th>
+                        <th scope="col">Updated At</th>
+                        <th scope="col">Created At</th>
+                        <th scope="col">Actions</th>
+                        
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>Otto</td>
+                        <td>Otto</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>Otto</td>
+                        <td>Otto</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                        <td>Otto</td>
+                        <td><div><button type="button" class="btn btn-primary">E</button>  <button type="button" class="btn btn-danger">D</button></div></td>
+                      </tr>
+                    </tbody>
+                  </table>
+            </div>
+        
         </div>
     </div>
 </div>
